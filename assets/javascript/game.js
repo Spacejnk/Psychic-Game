655599be8768ec25e68611ded5c507f6d944dc6d
Timits1 <will4f21@yahoo.com>
2019-06-24 04:55:11 -0700

    create function to increase score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,18 @@
-alert("Hello World!");
-console.log("Hello World!");
+// alert("Hello World!");
+// console.log("Hello World!");
+
+
+var timesClicked = 0;
+
+function btnClick(){
+    timesClicked ++;
+
+    document.getElementById("timesClicked").innerHTML = timesClicked;
+    return true;
+}
+
+if(timesClicked === 10){
+    alert("Over");
+} else if(timesClicked === 4){
+    alert("Really over!")
+}
