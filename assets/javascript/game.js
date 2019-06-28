0e87547c96f545040bf09cbc111f8388201066d4
Timits1 <will4f21@yahoo.com>
2019-06-28 04:03:58 -0700

    changed math.random formula

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,22 @@
-
-var timesClicked = -8;
+var timesClicked = 8;
 var xtimesClicked = 0;
-var guesses = [8];
+var wins = 0;
 var losses = 0;
+var guesses;
 
-// var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l',
-// 'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
-// var computerChoice = alpha[Math.floor(Math.random() * alpha.length)];
-
-// console.log(word)
-
+var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l',
+ 'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+ 
+var word = "";
 
 
 // wins function
 function winClick(){
-    xtimesClicked ++;
+    xtimesClicked --;
 
     document.getElementById("winClick").innerHTML = xtimesClicked;
     return true;
 }
-
-// guesses function
-function guessClick(){
-    timesClicked ++;
-
-    if(timesClicked == 1) {
-       console.log("game over!");
-       document.location.reload();
-       alert("game over!");
-    }
-    // else{
-    //     console.log('dcontinue');
-    // }
-
-    document.getElementById("guessClick").innerHTML = timesClicked;
-    return true;
-}
-
 // losses function
 function lostClick(){
     xtimesClicked ++;
@@ -45,41 +24,56 @@ function lostClick(){
     document.getElementById("lostClick").innerHTML = xtimesClicked;
     return true;
 }
+// guesses function
+function guessClick(){
+    timesClicked --;
 
+    if(timesClicked == 0) {
+       console.log("game over!");
+       document.location.reload();
+       alert("game over!");
+    }
+
+    document.getElementById("guessClick").innerHTML = timesClicked;
+    return true;
+}
 // word function
-var word = "";
 function dump(event) {
     var unicode = event.keyCode? event.keyCode : event.charCode;
-    var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l',
-    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     var computerChoice = alpha[Math.floor(Math.random() * alpha.length)];
-    var actualkey = String.fromCharCode(computerChoice);
-    word +=  computerChoice + ", ";
-    computerChoice === guessClick();
+    var actualkey = String.fromCharCode(alpha);
+    word +=  alpha + ", ";
+    alpha === guessClick();
 
+   //var elem = document.getElementById('lettersClicked').innerHTML += alpha;
+   var elem = document.getElementById('lettersClicked').innerHTML += word;
 
-    var elem = document.getElementById('lettersClicked');
+   
+   
+   
+   
+//    console.log(alpha);
+//    console.log(word);
+   
+   
+   
+   
+   
+   
+   
+//    var elem = document.getElementById('lettersClicked')
     
     
     
-    if(elem.innerHTML.indexOf(computerChoice) > -1 ){
-
-    lostClick();
-        
-    } else{
-    winClick();
-
-    }
     
-    
-    var targetLetters = document.getElementById( 'lettersClicked' );
+//     var targetLetters = document.getElementById( 'lettersClicked' );
 
-    // To get the text only, you can use "textContent"
-    console.log( targetLetters.textContent ); // "1 2 3 4"
+//     // To get the text only, you can use "textContent"
+//     console.log( targetLetters.textContent ); // "1 2 3 4"
 
 
-    document.getElementById("lettersClicked").innerHTML = word;
-    return true;
+//      document.getElementById("lettersClicked").innerHTML.textContent = alpha[i];
+//      return true;
 
 }
 document.onkeypress = dump;
@@ -90,3 +84,114 @@ document.onkeypress = dump;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // wins function
+// function winClick(){
+//     xtimesClicked ++;
+
+//     document.getElementById("winClick").innerHTML = xtimesClicked;
+//     return true;
+// }
+
+// // guesses function
+// function guessClick(){
+//     timesClicked ++;
+
+//     if(timesClicked == 1) {
+//        console.log("game over!");
+//        document.location.reload();
+//        alert("game over!");
+//     }
+//     // else{
+//     //     console.log('dcontinue');
+//     // }
+
+//     document.getElementById("guessClick").innerHTML = timesClicked;
+//     return true;
+// }
+
+// // losses function
+// function lostClick(){
+//     xtimesClicked ++;
+
+//     document.getElementById("lostClick").innerHTML = xtimesClicked;
+//     return true;
+// }
+
+// // word function
+// var word = "";
+// function dump(event) {
+//     var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l',
+//     'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+//     var computerChoice = alpha[Math.floor(Math.random() * alpha.length)];
+//     var actualkey = String.fromCharCode(computerChoice);
+//     word +=  computerChoice + ", ";
+//     computerChoice === guessClick();
+//     console.log()
+
+
+//     var elem = document.getElementById('lettersClicked');
+    
+    
+    
+//     if(elem.innerHTML.indexOf(computerChoice) > -1 ){
+
+//     lostClick();
+        
+//     } else{
+//     winClick();
+
+//     }
+    
+    
+//     var targetLetters = document.getElementById( 'lettersClicked' );
+
+//     // To get the text only, you can use "textContent"
+//     console.log( targetLetters.textContent ); // "1 2 3 4"
+
+
+//     document.getElementById("lettersClicked").innerHTML = word;
+//     return true;
+
+// }
+// document.onkeypress = dump;
